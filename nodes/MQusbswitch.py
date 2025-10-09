e9ae634264f11e2dcfbf49a57648c1ca266be285
tmorse305 <38409250+tmorse305@users.noreply.github.com>
2025-10-09 08:31:03 -0400

    Update MQusbswitch.py

--- nodes/MQusbswitch.py
+++ nodes/MQusbswitch.py
@@ -70,8 +70,31 @@ class MQusbswitch(udi_interface.Node):
 
     def cmd_on(self, command):
         self.on = True
-        self.setDriver("ST", 100)
+        #self.setDriver("ST", 100)
         #self.controller.mqtt_pub(self.cmd_topic, "ON")
+        
+        @ewelink.login('Tea4twoA@eWL','tmorse305@comcast.net')
+        async def main(client: Client):
+            print(client.region)
+            print(client.user.info)
+            print(client.devices)
+
+            device =  client.get_device('100118f515') #Mickey's ID
+            #print(device.params)
+                # Raw device specific properties
+                # can be accessed easily like: device.params.switch or device.params['startup'] (a subclass of dict)
+
+            #print(device.state)
+            #print(device.created_at)
+            #print("Brand Name:", device.brand.name, "Logo URL:", device.brand.logo.url)
+            #print("Device online?", device.online)
+
+            try:
+                #await device.on()
+                await device.edit(Power.on[0])
+                print("Power on sent")
+            except DeviceOffline:
+                print("Device is offline!")
 
     def cmd_off(self, command):
         self.on = False
