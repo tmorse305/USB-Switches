ac9dc6f492c124f523f245d3390068824d7c8cc0
tmorse305 <38409250+tmorse305@users.noreply.github.com>
2025-10-22 22:03:13 -0400

    Update MQusbswitch.py

--- nodes/MQusbswitch.py
+++ nodes/MQusbswitch.py
@@ -72,7 +72,7 @@ class MQusbswitch(udi_interface.Node):
             
     def cmd_on(self, command):
         self.on = True
-        # self.setDriver("ST", 100)
+        self.setDriver("ST", 100)
         # self.controller.mqtt_pub(self.cmd_topic, "ON")
         
         @ewelink.login('Tea4twoA@eWL','tmorse305@comcast.net')       
