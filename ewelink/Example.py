73ee8e521bd2c414b1741d5cf305975e24ba5760
tmorse305 <38409250+tmorse305@users.noreply.github.com>
2025-10-24 20:06:42 -0400

    Update Example.py

--- ewelink/Example.py
+++ ewelink/Example.py
@@ -1,33 +1,26 @@
-import ewelink
-from ewelink import Client, DeviceOffline, Power
-
-import sys
-
-# Print the sys.path variable
-print("sys.path:")
-for path in sys.path:
-    print(path)
-
-
-@ewelink.login('Tea4twoA@eWL','tmorse305@comcast.net')
-async def main(client: Client):
-    print(client.region)
-    print(client.user.info)
-    print(client.devices)
-
-    device =  client.get_device('100118ee7e') #Mickey's ID
-    print(device.params)
-        # Raw device specific properties
-        # can be accessed easily like: device.params.switch or device.params['startup'] (a subclass of dict)
-
-    print(device.state)
-    print(device.created_at)
-    print("Brand Name:", device.brand.name, "Logo URL:", device.brand.logo.url)
-    print("Device online?", device.online)
-
-    try:
-        #await device.on()
-        await device.edit(Power.off [0])
-        print("Power on sent")
-    except DeviceOffline:
-        print("Device is offline!")
+def call_dev(action, dev_id):
+        @ewelink.login(self.controller.getUSBPW(),self.controller.getUSBUSR())       
+        async def main(client: Client):
+            print(client.region)
+            print(client.user.info)
+            print(client.devices)
+                
+            device =  client.get_device(dev_id) #sonoff switch ID
+            global usb_sw_state
+            print(device.params)
+                # Raw device specific properties
+                # can be accessed easily like: device.params.switch or device.params['startup'] (a subclass of dict)
+    
+            print(device.state)            
+            print(device.created_at)
+            print("Brand Name:", device.brand.name, "Logo URL:", device.brand.logo.url)
+            print("Device online?", device.online)
+            usb_sw_state = device.state
+            if action != 'stay':
+                try:
+                    # await device.on()
+                    await device.edit(Power.off[0])
+                    print("Power on sent")
+                except DeviceOffline:
+                    print("Device is offline!")
+                print(device.state)
