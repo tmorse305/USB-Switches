86ae3bf5258fee3791ee9bea3f225184a5eea5b0
tmorse305 <38409250+tmorse305@users.noreply.github.com>
2025-10-25 13:16:07 -0400

    Update MQusbswitch.py

--- nodes/MQusbswitch.py
+++ nodes/MQusbswitch.py
@@ -102,7 +102,7 @@ class MQusbswitch(udi_interface.Node):
                 print("Power on sent")
             except DeviceOffline:
                 print("Device is offline!")
-            if device.online == 'True'
+            if device.online == 'True':
                 value = 1
             else value = 0
             self.setDriver("GV1", value) 
